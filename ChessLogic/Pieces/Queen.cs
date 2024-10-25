b3963e8248b5235302ece16323bb2788ad0e771d
Ilja Jezkovs <St62866@students.tsi.lv>
2024-10-25 13:37:19 +0300

    First commit, finally he is working (git hub working, not me or code)

---
+++ ChessLogic/Pieces/Queen.cs
@@ -0,0 +1,32 @@
+﻿namespace ChessLogic
+{
+    public class Queen : Piece
+    {
+        public override PieceType Type => PieceType.Queen;
+        public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North, Direction.NorthWest, Direction.NorthEast,
+            Direction.South, Direction.SouthWest, Direction.SouthEast,
+            Direction.West, Direction.East
+        };
+
+        public Queen(Player color)
+        {
+            Color = color;
+        }
+
+        public override Piece Copy()
+        {
+            Queen copy = new Queen(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        { 
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to)); 
+        }
+    }
+}
