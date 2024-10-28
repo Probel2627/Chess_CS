771072b405fa2eab9bfc7afb26c436c6e5fb5e79
Ilja Jezkovs <St62866@students.tsi.lv>
2024-10-28 18:07:59 +0300

    Pawn, Knight, King methods + Gate state adding moves Pawn - forward + diagonal Knight - 2+1 moving to all sides King - check around + move

--- ChessLogic/Pieces/Knight.cs
+++ ChessLogic/Pieces/Knight.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public class Knight : Piece
     {
@@ -23,5 +17,27 @@ namespace ChessLogic
             return copy;
         }
 
+        private static IEnumerable<Position> PotentialToPositions(Position from)  //Help method 
+        {
+            foreach (Direction vDir in new Direction[] { Direction.North, Direction.South }) //vertical Direction
+            {
+                foreach (Direction hDir in new Direction[] { Direction.West, Direction.East }) // horizontal Direction
+                {
+                    yield return from + 2 * vDir + hDir; //give all 8 potential positions
+                    yield return from + 2 * hDir + vDir;
+                }
+            }
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            return PotentialToPositions(from).Where(pos => Board.IsInside(pos)
+                && (board.IsEmpty(pos) || board[pos].Color != Color));
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositions(from, board).Select(to => new NormalMove(from, to));
+        }
     }
 }
--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3963e8248b5235302ece16323bb2788ad0e771d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
